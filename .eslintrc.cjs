11ee90b2d5313e53f0d33a75999f3037530f98df
lenghak <125646707+Lenghak@users.noreply.github.com>
2023-12-01 09:00:14 +0700

    Initial Commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,57 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  extends: ["plugin:astro/recommended"],
+  plugins: ["@typescript-eslint", "astro"],
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: path.join(__dirname, "tsconfig.json"),
+      },
+    },
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+        
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    extraFileExtensions: [".astro"],
+    // The script of Astro components uses ESM.
+    sourceType: "module",
+    project: path.join(__dirname, "tsconfig.json"),
+  },
+  rules: {
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-misused-promises": [
+      2,
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
+  },
+  ignorePatterns: [".eslintrc.cjs"],
+};
+
+module.exports = config;
---
+++ .prettierrc.cjs
@@ -0,0 +1,55 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "css",
+  jsxSingleQuote: false,
+  singleAttributePerLine: true,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  parser: "typescript",
+  plugins: [
+    require.resolve("@trivago/prettier-plugin-sort-imports"),
+    require.resolve("prettier-plugin-astro"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+  importOrder: [
+    "^@/modules/(.*)$",
+    "^@/common/components/(.*)$",
+    "^@/common/hooks/(.*)$",
+    "^@/common/lib/(.*)$",
+    "^@nexui-org/(.*)$",
+    "<THIRD_PARTY_MODULES>",
+    "^[./]",
+    "^@/common/types/(.*)$",
+    "^@/common/scss/(.*)$",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderCaseInsensitive: true,
+  overrides: [
+    {
+      files: "*.scss",
+      options: { parser: "scss" },
+    },
+    {
+      files: "*.json",
+      options: {
+        parser: "json",
+      },
+    },
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+    {
+      files: "*.md",
+      options: {
+        parser: "markdown",
+      },
+    },
+  ],
+};
